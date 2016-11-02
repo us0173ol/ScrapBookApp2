a2c2c411c85fae0098f97574d04ed2a33ab4046f
miked <whatthafunk3@aol.com>
2016-11-02 02:47:32 -0500

    not sure what I did

---
+++ app/src/main/java/com/bignerdranch/android/scrapbookapp2/DatabaseManager.java
@@ -0,0 +1,85 @@
+package com.bignerdranch.android.scrapbookapp2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by miked on 11/2/2016.
+ */
+
+public class DatabaseManager {
+
+    private Context mContext;
+    private SQLHelper mHelper;
+    private SQLiteDatabase db;
+
+    protected static final String DB_NAME = "stuff_and_things";
+    protected static final int DB_VERSION = 1;
+    protected static final String DB_TABLE = "pics_and_comments";
+
+    protected static final String ID_COL= "_id";
+    protected static final String PICTURE_COL = "pictures";
+    protected static final String COMMENTS_COL = "comments";
+    protected static final String TAGS_COL = "tags";
+    protected static final String DATE_MODIFIED_COL = "date_modified";
+
+    private static final String DB_TAG = "DatabaseManager";
+    private static final String SQLTAG = "SQLHelper";
+
+    public DatabaseManager(Context context){
+        this.mContext = context;
+        mHelper = new SQLHelper(context);
+        this.db = mHelper.getWritableDatabase();
+
+    }
+    public void close(){
+        mHelper.close();//closes the database
+    }
+    public Cursor getAllInfo(){
+        Cursor cursor = db.query(DB_TABLE,null, null, null, null, null, ID_COL);
+        return cursor;
+    }
+
+    public boolean addObject(String comment){
+        ContentValues newObject = new ContentValues();
+        newObject.put(COMMENTS_COL, comment);
+
+
+
+        try{
+            db.insertOrThrow(DB_TABLE, null, newObject);
+            Log.d(DB_TAG, "Added Comment:" + comment);
+            return true;
+        }catch(SQLiteConstraintException sqlce){
+            Log.e(DB_TAG, "error inserting data into table. " + comment +
+                    " was not inserted");
+            return false;
+        }
+    }
+    public class SQLHelper extends SQLiteOpenHelper {
+        public SQLHelper(Context context){
+            super(context, DB_NAME, null, DB_VERSION);
+
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            String createSQLbase = "CREATE TABLE %s ( %s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s TEXT, %s TEXT)";
+            String createSQL = String.format(createSQLbase, DB_TABLE, ID_COL, COMMENTS_COL, PICTURE_COL, TAGS_COL );
+            db.execSQL(createSQL);
+
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
+            onCreate(db);
+            Log.w(SQLTAG, "Upgrade table - drop and recreate it");
+        }
+    }
+}
+
---
+++ app/src/main/java/com/bignerdranch/android/scrapbookapp2/MainListCursorAdapter.java
@@ -0,0 +1,62 @@
+package com.bignerdranch.android.scrapbookapp2;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by miked on 11/2/2016.
+ */
+
+public class MainListCursorAdapter extends CursorAdapter {
+
+    private static final String TAG = "MAIN LIST CURSOR ADAPTER";
+    ItemContentsChangedListener mItemContentsChangedListener;
+
+    private static final int ID_COL = 0;
+    private static final int PICTURE_COL = 2;
+    private static final int COMMENTS_COL = 1;
+    private static final int TAGS_COL = 3;
+//    private static final int
+//    private static final int
+
+    public MainListCursorAdapter(Context context, Cursor cursor, boolean autoReQuery){
+        super(context, cursor, autoReQuery);
+
+        if (context instanceof ItemContentsChangedListener){
+            this.mItemContentsChangedListener = (ItemContentsChangedListener) context;
+
+        }else{
+            throw new RuntimeException(context.toString() + " must implement ItemContentsChangedListener");
+        }
+
+    }
+    public View newView(Context context, Cursor cursor, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.pictures_comments_list_item, parent, false);
+        return v;
+    }
+    public void bindView(View view, Context context, final Cursor cursor){
+        //TODO FINISH bindView
+        //ImageView pictureTaken = (ImageView) view.findViewById(R.id.item_list_picture_image_view);
+        TextView commentsMade = (TextView) view.findViewById(R.id.item_list_comment_text_view);
+        TextView tagsMade = (TextView) view.findViewById(R.id.item_list_tags);
+        //Bitmap bitmap = MainActivity.scaleBitmap();
+
+        tagsMade.setText(cursor.getString(TAGS_COL));
+        commentsMade.setText(cursor.getString(COMMENTS_COL));
+//        pictureTaken.setImageBitmap();
+
+        final int object_id = cursor.getInt(ID_COL);
+
+    }
+
+    interface ItemContentsChangedListener {
+        void notifyItemsChanged(int objectID, String newComments);
+    }
+}
+
